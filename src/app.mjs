625bf1d7458ec2e1ab38cf9a28ebfe9f7bd4c746
Engr-Pirlo <omarjibrilabdulkhadir@gmail.com>
2023-09-08 22:41:02 +0300

    first commit

---
+++ src/app.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+import { port } from "./config/config.js";
+import connectDb from "./config/db.js";
+import chalk from "chalk";
+
+const PORT = port || 3000
+
+const app = express()
+
+connectDb()
+
+app.listen(PORT, () => console.log(`${chalk.green.bold('Server')} is Running on port  ${chalk.green.bold(PORT)}`))
---
+++ src/models/post.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        require:true
+    },
+    content:{
+
+        type:String,
+        require: true
+    },
+
+    author:{
+        type: mongoose.Schema.Types.ObjectId
+    }
+})
